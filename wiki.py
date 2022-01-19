0c07f2c343c885d77c754fa6e42079ac4414e342
Caleb <jiale.hou@my.jcu.edu.au>
2022-01-19 11:47:33 +0800

    Add print page title, summary and url.

--- wiki.py
+++ wiki.py
@@ -2,5 +2,10 @@ import wikipedia
 
 page_title = input("Enter the page title: ")
 while page_title != '':
-    print(wikipedia.summary(f"{page_title}"))
+    page = wikipedia.page(f'{page_title}')
+    print(page.title)
+    print(page.summary)
+    print(page.url)
     page_title = input("Enter the page title: ")
+
+print(wikipedia.search('Monty'))
